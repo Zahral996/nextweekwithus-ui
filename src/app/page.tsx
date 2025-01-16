131146c0a96488f68655c1ac7aa8723a9ee53632
Zahra Bidkhori <zahrabidkhori@yahoo.com>
2025-01-16 18:01:32 +0330

    join the adventure was added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Category from "@/components/Landing/Category/Category";
 import Features from "@/components/Landing/Features/features";
 import HomeHero from "@/components/Landing/HomeHero/HomeHero";
 import HowWorks from "@/components/Landing/HowWorks/HowWorks";
+import Join from "@/components/Landing/Join/Join";
+
 
 export default function Home() {
   return (
@@ -9,6 +11,7 @@ export default function Home() {
       <HomeHero/>
       <Category/>
       <HowWorks/>
+      <Join/>
       <Features/>
     </div>
   );
---
+++ src/components/Landing/Join/Join.tsx
@@ -0,0 +1,22 @@
+import Button from "@/components/Shared/Button/Button";
+import Image from "next/image";
+
+
+const Join = () => {
+    return (
+        <div className="join container mx-auto min-h-[265px] relative flex flex-col md:flex-row justify-around items-center gap-y-2 lg:px-10 py-2">
+            <div className="content w-full md:w-6/12 flex flex-col gap-y-2 justify-center px-5">
+                <h2 className="text-xl font-extrabold">Join the Adventure!</h2>
+                <h3 className="font-semibold text-base">Ready to explore new horizons and create unforgettable memories?</h3>
+                <p className="hidden md:block text-base font-normal pb-2">Discover exciting events happening near you and connect with fellow adventurers! Sign up now to explore unique journeys, share your experiences, and create lasting memories.</p>
+                <Button href={"/signup"} btnName={"Sign up"}/>
+            </div>
+            <div className="flex justify-center w-full mt-5 lg:mt-0 lg:w-6/12  lg:justify-end">
+                <Image width={1000} height={1000} className="w-full md:w-[90%] 2xl:w-[80%]" src={"/images/landingPage/join/nextweekwithus_international-tourism-banner.webp"} alt="join the adventure" />
+            </div>
+    
+        </div>
+    );
+}
+
+export default Join;
