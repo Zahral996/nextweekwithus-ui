be7daf292cca5ff689e12c437717a763ca36d774
Zahra Bidkhori <zahrabidkhori@yahoo.com>
2025-01-27 10:21:16 +0330

    fix type error in request file

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,12 +3,12 @@ import {routing} from './routing';
  
 export default getRequestConfig(async ({requestLocale}) => {
   // This typically corresponds to the `[locale]` segment
-  let locale = await requestLocale;
+  let locale: string | undefined = await requestLocale;
  
   // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale as any)) {
+  if (!locale || !routing.locales.includes(locale as typeof routing.locales[number])) {
     locale = routing.defaultLocale;
-  }
+}
  
   return {
     locale,
